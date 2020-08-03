64e0fb8182a6a1a0e310cf588161a2dc414c7455
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-03 18:28:42 -0300

    Exporting authReducer

--- context/auth/authState.js
+++ context/auth/authState.js
@@ -1,9 +1,32 @@
 import React, { useReducer } from "react";
 
 import authContext from "./authContext";
+import authReducer from "./authReducer";
 
 const AuthState = ({ children }) => {
-  return <authContext.Provider value={{}}>{children}</authContext.Provider>;
+  // Initial State
+  const initialState = {
+    token: "",
+    authenticated: null,
+    user: null,
+    message: null,
+  };
+
+  // Define reducer
+  const [state, disptach] = useReducer(authReducer, initialState);
+
+  return (
+    <authContext.Provider
+      value={{
+        token: state.token,
+        authenticated: state.authenticated,
+        user: state.user,
+        message: state.message,
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  );
 };
 
 export default AuthState;

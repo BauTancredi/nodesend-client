acbddbae70b7b9c73425bc3d8523780936865c96
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-03 14:39:09 -0300

    Using formik and yup on login

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,96 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import Layout from "../components/Layout";
 
 const Login = () => {
+  // Form validarion with formik and yup
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("The email is not valid")
+        .required("The email is mandatory"),
+      password: Yup.string().required("The password cannot be empty"),
+    }),
+    onSubmit: () => {
+      console.log("Sending Form");
+    },
+  });
   return (
     <Layout>
-      <h1>Login</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">
+          Login
+        </h2>
+
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-lg">
+            <form
+              className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+              onSubmit={formik.handleSubmit}
+            >
+              <div className="mb-4">
+                <label
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  placeholder="Email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                    <p className="font-bold">Error</p>
+                    {formik.errors.email}
+                  </div>
+                ) : null}
+              </div>
+
+              <div className="mb-4">
+                <label
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  placeholder="Password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4">
+                    <p className="font-bold">Error</p>
+                    {formik.errors.password}
+                  </div>
+                ) : null}
+              </div>
+
+              <input
+                type="submit"
+                className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold"
+                value="Login"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };

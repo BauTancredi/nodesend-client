544dc1b2bb6636a88dd13c13a27bb56ed251d46d
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-05 18:01:49 -0300

    Uploading files to server

--- components/Dropzone.js
+++ components/Dropzone.js
@@ -3,8 +3,21 @@ import { useDropzone } from "react-dropzone";
 import clientAxios from "../config/axios";
 
 const Dropzone = () => {
+  const onDrop = useCallback(async (acceptedFiles) => {
+    //Create form data
+    const formData = new FormData();
+    formData.append("file", acceptedFiles[0]);
+
+    const result = await clientAxios.post("/api/files", formData);
+  }, []);
+
   //Extract content from dropzone
-  const { getRootProps, getInputProps, isDragActive } = useDropzone();
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    acceptedFiles,
+  } = useDropzone({ onDrop });
 
   return (
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 p-4">

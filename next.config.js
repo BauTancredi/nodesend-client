c9074e8aecea2961b486333bc979cee88ad54781
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-10 17:34:00 -0300

    testing cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    backendUrl: "https://dashboard.heroku.com/apps/salty-crag-54070",
-    frontendUrl: "https://admiring-jones-9b5088.netlify.app",
+    backendUrl: "https://salty-crag-54070.herokuapp.com/",
+    frontendUrl: "https://admiring-jones-9b5088.netlify.app/",
   },
 };

dcb71241e40606161ec93844ae965553f59b1d84
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-09-14 19:38:15 -0300

    Modified frontendUrl

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     backendUrl: "https://salty-crag-54070.herokuapp.com/",
-    frontendUrl: "https://admiring-jones-9b5088.netlify.app/",
+    frontendUrl: "https://nodesend-client-six.vercel.app/",
   },
 };
